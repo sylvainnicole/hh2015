ab1e16d50b1242317a607907eaf3424224722bff
sylvainnicole <xylozork@gmail.com>
2015-11-07 21:43:35 -0500

    test

--- server/api/nutri/index.js
+++ server/api/nutri/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var menuparser = require("./menuparser.js");
-var menu = new menuparser("../config/menuitems.json");
-
-var express = require('express');
-var router = express.Router();
-//console.log(menu.retrieveItemPerType("soup"));
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' }) });
-router.get('/:type', function(req, res) {
-    res.json(menu.retrieveItemPerType(req.params.type)) });
-
-module.exports = router;
+// var menu = new menuparser("../../config/menuitems.json");
+// 
+// var express = require('express');
+// var router = express.Router();
+// //console.log(menu.retrieveItemPerType("soup"));
+// router.get('/', function(req, res) {
+//     res.json({ message: 'hooray! welcome to our api!' }) });
+// router.get('/:type', function(req, res) {
+//     res.json(menu.retrieveItemPerType(req.params.type)) });
+// 
+// module.exports = router;
